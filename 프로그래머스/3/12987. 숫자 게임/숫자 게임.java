a6ff30031a3a5ea91e822c91ca5c3bff4d68135c
우승화 (WooSH) <62339794+ImGdevel@users.noreply.github.com>
2025-02-18 11:53:28 +0900

    [level 3] Title: 숫자 게임, Time: 64.78 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,30 +1,19 @@
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
-        // 그리디 문제 아닌가?
-        
-        TreeMap<Integer, Integer> tree = new TreeMap<>();
-        for(int n : B){
-            tree.put(n, tree.getOrDefault(n,0) + 1);
-        }
-        
-        // 
-        for(int n : A){
-            Integer best = tree.higherKey(n);
-            if(best == null){
-                // 가장 낮은 점수 감소
-                best = tree.firstKey();
-            }else{
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = A.length - 1, j = B.length - 1; i >= 0; i--) {
+            if(A[i] < B[j]) {
                 answer++;
-            }
-            
-            tree.put(best, tree.get(best) - 1);
-            if(tree.get(best) == 0){
-                tree.remove(best);
+                j--;
             }
         }
+
         return answer;
     }
 }
