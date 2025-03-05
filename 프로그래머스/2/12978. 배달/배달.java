c96247dd2989955a183eb350881f9dbe30beaf52
우승화 (WooSH) <62339794+ImGdevel@users.noreply.github.com>
2025-03-06 01:06:16 +0900

    [level 2] Title: 배달, Time: 8.65 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,59 +1,40 @@
 import java.util.*;
 
 class Solution {
-    
-    public int[] dickstra(List<int[]>[] graph, int N, int start){
-        
-        Queue<Integer> pq = new PriorityQueue<>();
-        int[] dist = new int[N];
-        Arrays.fill(dist, 1000000000);
-        
-        pq.offer(start);
-        dist[start] = 0;
-        
-        while(!pq.isEmpty()){
-            int u = pq.poll();    
-            int value = dist[u];
-            
-            for(int[] next : graph[u]){
-                int v = next[0];
-                int w = value + next[1];
-                
-                if(dist[v] > w){
-                    dist[v] = w;
-                    pq.offer(v);
-                }
-            }   
-        }
-        
-        return dist;
-    }
-    
-    
-    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        List<int[]>[] graph = new List[N];
+        
+        int[][] floyd = new int[N][N];
         for(int i=0; i<N; i++){
-            graph[i] = new ArrayList<>();
+            Arrays.fill(floyd[i], Integer.MAX_VALUE);
+            floyd[i][i] = 0;
         }
         
-        
-        
-        for(int[] n : road){
-            int s = n[0] - 1;
-            int e = n[1] - 1;
-            int w = n[2];
-            graph[s].add(new int[]{e,w});
-            graph[e].add(new int[]{s,w});
+        for(int[] r : road){
+            int s = r[0] - 1;
+            int e = r[1] - 1;
+            int w = r[2];
+            
+            floyd[s][e] = Math.min(w, floyd[s][e]);
+            floyd[e][s] = Math.min(w, floyd[s][e]);
         }
         
-        int[] dist = dickstra(graph, N, 0);
-        for(int n : dist){
+        for(int k=0; k<N; k++){
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if (floyd[i][k] != Integer.MAX_VALUE && floyd[k][j] != Integer.MAX_VALUE) {
+                        floyd[i][j] = Math.min(floyd[i][j], floyd[i][k] + floyd[k][j]);
+                    }
+                }
+            }
+        }
+        
+        for(int n : floyd[0]){
             if(n <= K){
                 answer++;
             }
         }
+        
 
         return answer;
     }
