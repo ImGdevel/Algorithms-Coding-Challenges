b835460b2b273141343f0923bea0045b1e98ce89
우승화 (WooSH) <62339794+ImGdevel@users.noreply.github.com>
2025-03-06 13:31:08 +0900

    [level 2] Title: 배달, Time: 12.17 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,41 +1,63 @@
 import java.util.*;
 
 class Solution {
+
+    class Edge{
+        int s;
+        int e;
+        int w;
+        
+        public Edge(int s, int e, int w){
+            this.s = s; this.e = e; this.w = w;
+        }
+    }
+    
+    int INF = Integer.MAX_VALUE;
+    
+    public int[] BF(List<Edge> eg, int N, int start){
+        
+        int[] dist = new int[N];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        
+        for(int i=0; i<N-1; i++){
+            for(Edge e : eg){
+                if(dist[e.s] != INF && dist[e.s] + e.w < dist[e.e]){
+                    dist[e.e] = dist[e.s] + e.w;
+                }
+                if(dist[e.e] != INF && dist[e.e] + e.w < dist[e.s]){
+                    dist[e.s] = dist[e.e] + e.w;
+                }
+                
+            }
+            //System.out.println(Arrays.toString(dist));
+        }
+        
+        return dist;
+    }
+    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        
-        int[][] floyd = new int[N][N];
-        for(int i=0; i<N; i++){
-            Arrays.fill(floyd[i], Integer.MAX_VALUE);
-            floyd[i][i] = 0;
-        }
+
+        List<Edge> eg = new ArrayList<>();
         
         for(int[] r : road){
             int s = r[0] - 1;
             int e = r[1] - 1;
             int w = r[2];
-            
-            floyd[s][e] = Math.min(w, floyd[s][e]);
-            floyd[e][s] = Math.min(w, floyd[s][e]);
+            eg.add(new Edge(s,e,w));
         }
         
-        for(int k=0; k<N; k++){
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
-                    if (floyd[i][k] != Integer.MAX_VALUE && floyd[k][j] != Integer.MAX_VALUE) {
-                        floyd[i][j] = Math.min(floyd[i][j], floyd[i][k] + floyd[k][j]);
-                    }
-                }
-            }
-        }
+        int[] dist = BF(eg, N, 0);
         
-        for(int n : floyd[0]){
-            if(n <= K){
+        //System.out.println(Arrays.toString(dist));
+        
+        for(int d : dist){
+            if(d <= K){
                 answer++;
             }
         }
         
-
         return answer;
     }
 }
