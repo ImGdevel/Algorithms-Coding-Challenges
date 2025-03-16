92904b31177d50e6c5b0f37f9e288932898d6710
우승화 (WooSH) <62339794+ImGdevel@users.noreply.github.com>
2025-03-16 17:30:19 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2692 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,33 @@
+//https://www.acmicpc.net/problem/2352
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N; 
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> LST;
+    for (int i = 0; i < N; i++) {
+        int pos = (lower_bound(LST.begin(), LST.end(), arr[i]) - LST.begin());
+        if (pos == LST.size()) {
+            LST.push_back(arr[i]);
+
+        }
+        else {
+            LST[pos] = arr[i];
+        }
+    }
+
+    cout << LST.size();
+
+    return 0;
+}
