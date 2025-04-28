c08956048911a78d6bdac78c2e6aead6b4695227
우승화 (WooSH) <imdlsrks.mc@gmail.com>
2025-04-28 19:47:10 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -5,35 +5,35 @@
 using namespace std;
 
 int main() {
-    int A, B;
-    cin >> A >> B;
+	int A, B;
+	cin >> A >> B;
+	
+	queue<int> q;
+	q.push(A);
 
-    queue<int> q;
-    q.push(A);
+	int count = 0;
+	while (!q.empty())
+	{
+		count++;
+		int size = q.size();
 
-    int count = 0;
-    while (!q.empty())
-    {
-        count++;
-        int size = q.size();
+		for (int i = 0; i < size; i++) {
+			int n = q.front();
+			q.pop();
 
-        for (int i = 0; i < size; i++) {
-            int n = q.front();
-            q.pop();
+			if (n == B) {
+				std::cout << count;
+				return 0;
+			}
 
-            if (n == B) {
-                cout << count;
-                return 0;
-            }
+			if ((long long)n * 2 <= B) {
+				q.push(n * 2);
+			}
 
-            if ((long long)n * 2 <= B) {
-                q.push(n * 2);
-            }
-
-            if ((long long)n * 10 + 1 <= B) {
-                q.push(n * 10 + 1);
-            }
-        }
-    }
-    cout << -1;
+			if ((long long)n * 10 + 1 <= B) {
+				q.push(n * 10 + 1);
+			}
+		}
+	}
+	std::cout << -1;
 }
