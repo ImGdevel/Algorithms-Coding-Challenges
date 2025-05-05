10fd59bed13618db288cb01ff383d7de8d1f01a7
우승화 (WooSH) <imdlsrks.mc@gmail.com>
2025-05-05 13:24:02 +0900

    [Gold IV] Title: 문자열 폭발, Time: 332 ms, Memory: 38576 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/// 알고리즘 코드를 작성해주세요 /////////////////////////////////////////
+
+import  java.util.*;
+
+
+class Solution {
+
+    public String solution(String origin, String target) {
+
+        StringBuilder str = new StringBuilder();
+
+        for(char ch : origin.toCharArray()){
+            str.append(ch);
+
+            if(ch == target.charAt(target.length() - 1) && str.length() >= target.length()){
+                // 문자열이 같은지 검사
+                if(target.equals(str.substring(str.length() - target.length(), str.length()))){
+                    str.delete(str.length() - target.length(), str.length());
+                }
+            }
+        }
+
+        if(str.toString().equals("")){
+            return "FRULA";
+        }
+        return str.toString();
+    }
+}
+
+/// 입출력을 정의해주세요 ///////////////////////////////////////////////
+
+public class Main {
+
+    static Solution solution = new Solution();
+
+    public static void main(String[] args) throws IOException {
+        // Input
+        String str1 = InputView.readString();
+        String str2 = InputView.readString();
+
+        // Solution
+        String ans = solution.solution(str1, str2);
+
+        //Output
+        OutputView.print(ans);
+    }
+}
+
+/// [ Input&Output View 라이브러리 ] //////////////////////////////////
+class InputView {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int readInt() throws IOException { return Integer.parseInt(br.readLine().trim()); }
+
+    public static int[] readIntArray() throws IOException { return Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray(); }
+
+    public static int[][] readIntMatrix(int n, int m) throws IOException { int[][] matrix = new int[n][m]; for (int i = 0; i < n; i++) { matrix[i] = readIntArray(); } return matrix; }
+
+    public static char readChar() throws IOException { return br.readLine().trim().charAt(0); }
+
+    public static char[] readCharArray() throws IOException { return br.readLine().trim().toCharArray(); }
+
+    public static char[][] readCharMatrix(int n, int m) throws IOException { char[][] matrix = new char[n][m]; for (int i = 0; i < n; i++) { matrix[i] = readCharArray(); } return matrix; }
+
+    public static String readString() throws IOException { return br.readLine().trim(); }
+
+    public static String[] readStringArray(int n) throws IOException { String[] array = new String[n]; for (int i = 0; i < n; i++) { array[i] = br.readLine().trim(); } return array; }
+}
+
+class OutputView {
+    public static void print(String message) { System.out.println(message); }
+
+    public static void print(int message) { System.out.println(message); }
+
+    public static void print(long message) { System.out.println(message); }
+
+    public static void print(int[] array) { for (int n : array) { System.out.print(n + " "); } }
+
+    public static void print(int[][] matrix) { for (int[] row : matrix) { for (int n : row) { System.out.print(n + " "); } System.out.println(); } }
+
+    public static void print(char[] array) { for (char ch : array) { System.out.print(ch + " "); } }
+
+    public static void print(char[][] matrix) { for (char[] row : matrix) { for (char ch : row) { System.out.print(ch + " "); } System.out.println(); } }
+
+    public static void print(String[] array) { for (String str : array) { System.out.println(str); } }
+
+    public static void print(List<String> array) { for (String str : array) { System.out.println(str); } }
+}
